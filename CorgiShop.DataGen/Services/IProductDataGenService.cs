d67a143c207e9159f9bf3bd2b69f6a857297d01b
Smithers <brandon.smithers@mythical.games>
2022-11-19 13:48:54 -0600

    Add product data generation

---
+++ CorgiShop.DataGen/Services/IProductDataGenService.cs
@@ -0,0 +1,8 @@
+﻿using CorgiShop.Repo.Model;
+
+namespace CorgiShop.DataGen.Services;
+
+public interface IProductDataGenService
+{
+    Task GenerateProducts(CorgiShopDbContext dbContext, int productCount);
+}
