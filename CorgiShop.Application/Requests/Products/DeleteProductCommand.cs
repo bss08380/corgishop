669afc6224b5efaf3b1046a8c2da407a2d0a6c8e
Smithers <brandon.smithers@mythical.games>
2022-11-20 18:21:25 -0600

    Add general swagger setup

--- CorgiShop.Application/Requests/Products/DeleteProductCommand.cs
+++ CorgiShop.Application/Requests/Products/DeleteProductCommand.cs
@@ -1,11 +1,11 @@
 ﻿using MediatR;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CorgiShop.Application.Requests.Products
-{
-    public  record DeleteProductCommand(int ProductId) : IRequest;
-}
+namespace CorgiShop.Application.Requests.Products;
+
+public record DeleteProductCommand([Required] int ProductId) : IRequest;
